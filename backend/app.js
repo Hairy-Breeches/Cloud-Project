ee0c7f70443d3a3c1b5bd10c3f62dd7e945896e9
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-06-05 05:11:34 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose
     console.log("Connection failed to database!");
   });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
